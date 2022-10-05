d225cf8fff9ce0780f57b83a54b9a7f3826ff2de
Kush Khanna <57548935+krash3125@users.noreply.github.com>
2022-10-05 09:04:47 -0700

    dock

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -2,15 +2,25 @@ import Launchpad from '../assets/launchpad.png';
 import Safari from '../assets/safari.png';
 import iMessage from '../assets/imessage.png';
 import VSCode from '../assets/vscode.png';
+import Github from '../assets/github.png';
 import DockItem from './DockItem';
+import { useContext } from 'react';
+import { WindowManagerContext } from '../context/WindowManager';
 
 const Dock = () => {
+  const { openWindow } = useContext(WindowManagerContext);
+
   return (
     <div className="h-dock mx-auto flex w-fit flex-row gap-3 rounded-xl border border-[#ffffff4a] bg-clear p-2 px-3">
-      <DockItem src={Launchpad} index={0} />
-      <DockItem src={Safari} index={1} />
-      <DockItem src={iMessage} index={2} />
-      <DockItem src={VSCode} index={3} />
+      <DockItem src={Launchpad} index={0} onClick={() => openWindow(0)} />
+      <DockItem src={Safari} index={1} onClick={() => openWindow(1)} />
+      <DockItem src={iMessage} index={2} onClick={() => openWindow(2)} />
+      <DockItem src={VSCode} index={3} onClick={() => openWindow(3)} />
+      <DockItem
+        src={Github}
+        index={4}
+        onClick={() => window.open('https://github.com/krash3125', '_blank')}
+      />
     </div>
   );
 };
--- src/components/DockItem.jsx
+++ src/components/DockItem.jsx
@@ -2,15 +2,13 @@ import { motion } from 'framer-motion';
 import { useContext } from 'react';
 import { WindowManagerContext } from '../context/WindowManager';
 
-const DockItem = ({ index, src }) => {
-  const { openWindow } = useContext(WindowManagerContext);
-
+const DockItem = ({ index, src, onClick }) => {
   return (
     <motion.img
       className="hover:cursor-pointer"
       whileHover={{ scale: 1.2 }}
       src={src}
-      onClick={() => openWindow(index)}
+      onClick={onClick}
     />
   );
 };
